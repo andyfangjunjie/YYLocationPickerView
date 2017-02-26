64aa7d6d26176e9ca763bb8351d73b93882eb003
房俊杰 <andyfangjunjie@163.com>
2017-02-26 15:01:04 +0800

    完成demo

--- YYLocationPickerViewDemo/YYLocationPickerViewDemo/ViewController.m
+++ YYLocationPickerViewDemo/YYLocationPickerViewDemo/ViewController.m
@@ -11,7 +11,13 @@
 
 
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate,YYLocationPickerViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *provinceTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *cityTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *areaTextField;
 
 @end
 
@@ -20,13 +26,66 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    
+    
+    
+    
+    
+}
+#pragma mark - UITextFieldDelegate
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+
+    if(textField == self.provinceTextField)
+    {
+        YYLocationPickerView *location = [[YYLocationPickerView alloc] initWithLevel:YYLocationPickerViewLevelProvince andDelegate:self];
+        location.tag = 1;
+        [location show];
+    }
+    if(textField == self.cityTextField)
+    {
+        YYLocationPickerView *location = [[YYLocationPickerView alloc] initWithLevel:YYLocationPickerViewLevelCity andDelegate:self];
+        location.tag = 2;
+        [location show];
+    }
+    if(textField == self.areaTextField)
+    {
+        YYLocationPickerView *location = [[YYLocationPickerView alloc] initWithLevel:YYLocationPickerViewLevelArea andDelegate:self];
+        location.tag = 3;
+        [location show];
+    }
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - YYLocationPickViewDelegate
+- (void)locationPickerView:(YYLocationPickerView *)locationPickerView didSelectedLocation:(NSString *)location locationID:(NSString *)locationID
+{
+    switch (locationPickerView.tag) {
+        case 1:
+        {
+            self.provinceTextField.text = [NSString stringWithFormat:@"%@-%@",location,locationID];
+            break;
+        }
+        case 2:
+        {
+            self.cityTextField.text = [NSString stringWithFormat:@"%@-%@",location,locationID];
+            break;
+        }
+        case 3:
+        {
+            self.areaTextField.text = [NSString stringWithFormat:@"%@-%@",location,locationID];
+            break;
+        }
+        default:
+            break;
+    }
 }
-
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [super touchesBegan:touches withEvent:event];
+    
+    [self.view endEditing:YES];
+}
 
 @end
